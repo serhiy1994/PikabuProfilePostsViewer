2581da2eb53a815b1e95d1d1abc0c5b73c252c50
Serhiy Mashkovskyi <mssss@ukr.net>
2019-06-10 14:58:32 +0300

    initial project version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Тоня
+ * Date: 02.05.2019
+ * Time: 17:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace PikabuProfilePostsViewer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.TextBox textBox2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(7, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(168, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Type a Pikabu user\'s name here:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(172, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(135, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(313, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 20);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Find posts";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.columnHeader1,
+			this.columnHeader2,
+			this.columnHeader3,
+			this.columnHeader4,
+			this.columnHeader5});
+			this.listView1.GridLines = true;
+			this.listView1.Location = new System.Drawing.Point(12, 204);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(1031, 233);
+			this.listView1.TabIndex = 3;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "#";
+			this.columnHeader1.Width = 30;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Date";
+			this.columnHeader2.Width = 90;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Rating";
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Title";
+			this.columnHeader4.Width = 420;
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "Link";
+			this.columnHeader5.Width = 420;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 39);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(943, 159);
+			this.textBox2.TabIndex = 4;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1053, 449);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "PikabuProfilePostsViewer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Net;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace PikabuProfilePostsViewer
+{
+	public partial class MainForm : Form
+	{
+		string username=string.Empty;
+		string url="https://pikabu.ru/@";
+		//HtmlWeb web = new HtmlWeb();
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			username=textBox1.Text;
+			if (username.Length<5) MessageBox.Show("Invalid username: too short");
+			else{
+				url+=username;
+				WebClient client = new WebClient(); //TODO: change to overrideencoding in HAP
+				var data = client.DownloadData(url);
+				var html = Encoding.GetEncoding("windows-1251").GetString(data);				
+				HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+				doc.LoadHtml(html);
+				textBox2.Text=doc.DocumentNode.OuterHtml;
+				string NoP =doc.DocumentNode.SelectSingleNode("//section[contains(@class, 'section_padding_none')]").
+					SelectSingleNode("(//div[@class='profile__section'])[2]").
+					SelectSingleNode("(//span[@class='profile__digital'])[2]").
+					SelectSingleNode("//b").InnerText;
+				MessageBox.Show(NoP.ToString());
+			}
+		}
+	}
+}
